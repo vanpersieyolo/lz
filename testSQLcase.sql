ee247e362301ed519eeeefd963cd6de9b08ccbea
= <=>
2020-09-24 21:23:33 +0700

    van

--- testSQLcase.sql
+++ testSQLcase.sql
@@ -1,34 +1,53 @@
 create database SellCar;
 use sellcar;
-create table cart(
-	cart_id int primary key auto_increment,
-    car_id int
+create table carts(
+	cart_id int primary key auto_increment
 );
-create table car(
+create table cars(
 car_id int primary key auto_increment,
 car_name varchar(120) not null,
 car_img varchar(255),
 car_price varchar(100),
-car_description varchar(255),
+car_description varchar(255)
+);
+create table orders(
 cart_id int,
-foreign key (cart_id) references cart(cart_id)
+car_id int,
+foreign key (cart_id) references carts(cart_id),
+foreign key (car_id) references cars(car_id)
 );
 
-
-insert into car (car_name,car_img,car_price,car_description) values ('hatback','img1.jpg','500','Huyndai');
-select * from car;
 
 delimiter $$
 create procedure delete_car(car_id int)
 	begin
-    delete from car where car.car_id = car_id;
+    delete from cars where cars.car_id = car_id;
     end $$
 delimiter ;
 
 delimiter $$
 create procedure get_car_by_id(car_id int)
 begin
-	select * from car 
-    where car.car_id = car_id;
+	select * from cars
+    where cars.car_id = car_id;
 end $$
 delimiter ;
+
+delimiter $$
+create procedure insert_car( in car_nameip varchar(255) ,in car_imgip varchar(255),in car_priceip varchar(100), in car_descriptionip varchar(255))
+begin
+insert into cars(car_name,car_img, car_price,car_description) values (car_nameip,car_imgip, car_priceip,car_descriptionip);
+end $$
+delimiter ;
+drop procedure insert_car;
+call insert_car('inova','img.jpg','1000$',' SUV');
+
+delimiter $$
+create procedure select_all()
+begin
+select * from cars;
+end $$
+delimiter ;
+
+call select_all();
+delete from cars where cars.car_id = 6;
